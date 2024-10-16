943fe8bfb57dacdeb2c5cd4239302d2b93406227
phello57 <lobanchikov.artem@gmail.com>
2024-10-16 11:45:15 +0300

    proxy echo

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rack', '3.1.8'
 gem 'rackup'
-
+gem 'faraday'
 gem 'puma'
 
 
--- controller.rb
+++ controller.rb
@@ -1,9 +1,16 @@
 require 'rack'
+require 'faraday'
+require 'json'
+
+echo_url = "https://echo.free.beeceptor.com/?"
 
 app = proc do |env|
-  [200, { 'Content-Type' => 'text/plain' }, [Rack::Request.new(env).params.to_s]]
+  add_params = Rack::Utils.build_query(Rack::Request.new(env).params)
+
+  response = Faraday.get(echo_url + add_params)
+
+  [200, { 'Content-Type' => 'text/plain' }, [JSON.parse(response.body)["parsedQueryParams"].to_s ]]
 end
 
 Rackup::Handler::WEBrick.run app
-
 
