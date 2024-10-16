bce2e1598f81643bd1348385ab112f88f8d63b12
phello57 <lobanchikov.artem@gmail.com>
2024-10-16 12:47:22 +0300

    proxy echo

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rack', '3.1.8'
-gem 'rackup'
 gem 'faraday'
 gem 'puma'
+gem 'rack', '3.1.8'
+gem 'rackup'
--- controller.rb
+++ controller.rb
@@ -1,11 +1,11 @@
 require 'rack'
 require 'faraday'
 
-ECHO_URL = "https://echo.free.beeceptor.com"
 
 class App
+  ECHO_URL = 'https://echo.free.beeceptor.com'
   def call(env)
-    response =  Faraday.post(ECHO_URL) do |req|
+    response = Faraday.post(ECHO_URL) do |req|
       req.headers['Content-Type'] = 'application/json'
       req.body = Rack::Request.new(env).params.to_json
     end
