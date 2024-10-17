99fc546ede0f3c17bc4b9f4bf8efcdf97c83b791
Artem <103268341+phello57@users.noreply.github.com>
2024-10-18 00:49:16 +0300

    Revert "1"

--- Gemfile
+++ Gemfile
@@ -1,10 +1,6 @@
-# frozen_string_literal: true
-
 source 'https://rubygems.org'
 
-gem 'bcrypt'
 gem 'faraday'
 gem 'puma'
 gem 'rack', '3.1.8'
 gem 'rackup'
-gem 'sqlite3', '~> 2.1'
--- controller.rb
+++ controller.rb
@@ -1,78 +1,22 @@
-# frozen_string_literal: true
-
 require 'rack'
 require 'faraday'
-require 'base64'
-require 'sqlite3'
-require 'bcrypt'
-require 'json'
+require "base64"
 
 class App
-  ECHO_URL = 'https://echo.free.beeceptor.com'
-  def initialize
-    @db = SQLite3::Database.new('test.db')
-    @request = nil
-  end
-
+  LOGIN_FROM_BD = 'phello'
+  PASS_FROM_BD = 'qwerty123'
   def call(env)
-    @request = Rack::Request.new(env)
 
-    if @request.get? && @request.path == '/token'
+    auth = Rack::Request.new(env).env["HTTP_AUTHORIZATION"]
 
-      # === User data ===
-      req_login, req_pass = parse_credentials
-      db_id, db_pass = fetch_user_data(req_login.to_sym.to_s)
-      !db_pass.nil? && BCrypt::Password.new(db_pass) == req_pass
+    log_pass = Base64.decode64(auth&.split&.last).split(':')
 
-      # === Token ===
-      json = { "id_user": db_id, "login": req_login }.to_json
-      token = Base64.encode64(json).delete("\n")
-
-      # === Result ===
-
-      json_responce = { token: token, userdata: json }.to_json
-
-      [200, { 'Content-Type' => 'application/json' }, [json_responce]]
+    if log_pass[0] == LOGIN_FROM_BD && log_pass[1] == PASS_FROM_BD
+      [200, { 'Content-Type' => 'text/plain' }, ["You authentication"]]
     else
-
-      json_string = @request.params.to_json
-      parsed_params = JSON.parse(json_string)
-
-      user_data = Base64.decode64(parsed_params['token'])
-      parsed_user_data = JSON.parse(user_data)
-
-      is_user_auth = fetch_user_exists(parsed_user_data) == 1
-
-      # === Echo ===
-      send_echo({ token: token }.to_json)
-
-
-      if is_user_auth
-        [200, { 'Content-Type' => 'text/plain' }, ['You authentication was successful']]
-      else
-        [401, { 'Content-Type' => 'text/plain' }, [parsed_user_data['login']]]
-      end
+      [401, { 'Content-Type' => 'text/plain' }, ["You aren`t authentication"]]
     end
-  end
 
-  def parse_credentials
-    auth = @request.env['HTTP_AUTHORIZATION']
-    Base64.decode64(auth.split&.last).split(':')
-  end
-
-  def send_echo(data)
-    Faraday.post(ECHO_URL) do |req|
-      req.headers['Content-Type'] = 'application/json'
-      req.body = data
-    end
-  end
-
-  def fetch_user_data(login)
-    @db.execute('select id, password from users where username = ? ', login).first
-  end
-
-  def fetch_user_exists(data)
-    @db.execute('select 1 from users where id = ? ', data['id_user']).first&.first
   end
 end
 
