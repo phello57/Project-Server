52818609ebe7c958748b5607346849ca2afc409b
phello57 <lobanchikov.artem@gmail.com>
2024-10-17 23:28:04 +0300

    token

--- sql/migrate.rb
+++ sql/migrate.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-class Migrate
-  def initialize(db)
-    db.execute('create table IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(20) UNIQUE, password VARCHAR(60))')
-  end
-end
+db.execute('create table IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(20) UNIQUE, password VARCHAR(60))')
+
+db.execute('create table IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, userid INTEGER UNIQUE, token VARCHAR(300))')
